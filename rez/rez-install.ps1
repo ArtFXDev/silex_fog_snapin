4c83b70b199dcdda09a41d444d5242b5da268474
OlivierArgentieri <olivierargentieri@gmail.com>
2021-11-22 14:19:24 +0100

    Update rez-install.ps1

--- rez/rez-install.ps1
+++ rez/rez-install.ps1
@@ -7,13 +7,13 @@ New-PSDrive -Name S -Root \\prod.silex.artfx.fr\rez\ -PSProvider FileSystem
 Remove-Item -Recurse -Force "c:\rez"
 Copy-Item S:\windows\rez-2.95.0 -destination "c:\rez\rez-2.95.0" -Recurse -Force
 
-# if not rez env
-if ($env:REZ_CONFIG_FILE.length -eq 0 ) {
-	[Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable('Path', 'Machine')+';C:\rez\__install__\Scripts\rez', [System.EnvironmentVariableTarget]::Machine)
-	[Environment]::SetEnvironmentVariable("REZ_CONFIG_FILE", "\\192.168.2.112\rez\windows\config\rezconfig.py", "Machine")
+$rez_in_path=$env:Path | Select-String "C:/rez/__install__/*"
+[Environment]::SetEnvironmentVariable("REZ_CONFIG_FILE", "\\192.168.2.112\rez\windows\config\rezconfig.py", "Machine")
+if ($rez_in_path.length -eq 0) {
+	[Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable('Path', 'Machine')+';C:/rez/__install__/Scripts/rez', [System.EnvironmentVariableTarget]::Machine)
 }
 
-# fix python install
+# fix python install and install rez
 $python_version=python --version | Select-String -Pattern "Python 2"
 if ($python_version.length -gt 0 -And -Not(Test-Path -Path "C:\Python37")) {
 	Copy-Item S:\windows\Python37 -destination "C:\Python37" -Recurse -Force
